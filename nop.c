2e3b9898ef90d403a4539bb716382f0c7af6d012
jobukk <jobkipkurui3843@gmail.com>
2024-02-01 13:44:58 -0800

    nop

--- nop.c
+++ nop.c
@@ -1,4 +1,12 @@
 #include "monty.h"
+/**
+ *nop - outputs nothing
+ *@stk: stack
+ *@line_number: num line
+ *
+ *
+ *Return: no return
+ */
 void nop(stack_t **stk, unsigned int line_number)
 {
 (void)stk;
